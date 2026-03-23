78c43200faec87213dca4004e120e7dae86718f4
Suhani Makati <suhanimakati07@gmail.com>
2026-03-23 17:11:40 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,11 @@
+#union of the elements
+set2 = {10,11,12}
+set_union = set.union(set2)
+print("union of set and set 2 :", set_union)
+#intersection of the elements
+set3 = {5,6,7,10}
+set_intersection = set.intersection(set3)
+print("intersection of set and set 3 :", set_intersection)
+#difference of the elements
+set_difference = set.difference (set3)
+print("difference of set and set 3 :", set_difference)
